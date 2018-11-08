6954a26e071cb4c7f633fd9fcee9523041e6a775
Raphael <raphaelrdsoares@gmail.com>
2018-11-07 23:47:00 -0300

    formatando o arquivo gulp;

--- gulpfile.js
+++ gulpfile.js
@@ -13,53 +13,51 @@ $ sudo npm install gulp - g
 4. Executar o comando '$ gulp build' para gerar uma versão de destribuição
 */
 
-const gulp = require('gulp');
-const browserSync = require('browser-sync')
-const minify = require('gulp-minify');
-const cssnano = require('gulp-cssnano');
-const gulpIf = require('gulp-if');
-const useref = require('gulp-useref')
-const del = require('del');
-const runSequence = require('run-sequence');
-const imagemin = require('gulp-imagemin');
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const minify = require("gulp-minify");
+const cssnano = require("gulp-cssnano");
+const gulpIf = require("gulp-if");
+const useref = require("gulp-useref");
+const del = require("del");
+const runSequence = require("run-sequence");
+const imagemin = require("gulp-imagemin");
 
 // ================= BUILD TASKS ====================
-gulp.task('clean', function () {
-    return del['dist', {force: true}];
-})
-
-gulp.task('images', function () {
-    return gulp.src('src/**/*.+(png|jpg|gif|svg)')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/'))
+gulp.task("clean", function() {
+	return del[("dist", { force: true })];
 });
 
-gulp.task('useref', function () {
-    return gulp.src('src/*.html')
-        .pipe(useref())
-        .pipe(gulpIf('*.js', minify({ noSource: true, ext: { min: '.js' }, }))) // Minifies only if it's a JavaScript file
-        .pipe(gulpIf('*.css', cssnano())) // Minifies only if it's a CSS file
-        .pipe(gulp.dest('dist'))
+gulp.task("images", function() {
+	return gulp
+		.src("src/**/*.+(png|jpg|gif|svg)")
+		.pipe(imagemin())
+		.pipe(gulp.dest("dist/"));
 });
 
-gulp.task('build', function (callback) {
-    runSequence('clean',
-        ['useref', 'images'],
-        callback
-    )
-})
+gulp.task("useref", function() {
+	return gulp
+		.src("src/*.html")
+		.pipe(useref())
+		.pipe(gulpIf("*.js", minify({ noSource: true, ext: { min: ".js" } }))) // Minifies only if it's a JavaScript file
+		.pipe(gulpIf("*.css", cssnano())) // Minifies only if it's a CSS file
+		.pipe(gulp.dest("dist"));
+});
+
+gulp.task("build", function(callback) {
+	runSequence("clean", ["useref", "images"], callback);
+});
 
 // ================= DEVELOPMENT TASKS ====================
-gulp.task('browserSync', function () {
-    return browserSync({
-        server: {
-            baseDir: ["./", "./src"]
-        },
-        port: 8080
-    });
+gulp.task("browserSync", function() {
+	return browserSync({
+		server: {
+			baseDir: ["./", "./src"]
+		},
+		port: 8080
+	});
 });
 
-
-gulp.task('start', ['browserSync'], function () {
-    gulp.watch('src/**/*.*', browserSync.reload);
+gulp.task("start", ["browserSync"], function() {
+	gulp.watch("src/**/*.*", browserSync.reload);
 });
