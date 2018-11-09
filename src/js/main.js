d1f8856912ce667cbae286d3584a26808db250d4
Raphael Felipe Ramos Duarte Soares <rfrds@intranet.pitang.com>
2018-11-09 18:50:08 -0300

    inclusão dos comentários com headers no arquivo css e js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
+/**
+ * ---------------------------------------------------
+ * Starter Template - Um template com estrutura inicial para um projeto básico front-end.
+ *
+ * @author Copyright 2018 RaphaelRDSoares <raphael@rdsoares.com>
+ * @license https://en.wikipedia.org/wiki/MIT_License
+ * @see https://github.com/raphaelrdsoares/starter-template
+ * @version 0.1.0
+ * ---------------------------------------------------
+ */
+
 $(document).ready(function () {
     
 });
